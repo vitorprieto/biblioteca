952de4def6bc07c2fde497d745834b26070a3116
2Fac3R <genr1818@gmail.com>
2021-11-17 17:37:52 -0600

    Books table added in categories/views/show

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -51,7 +51,42 @@
     </form>
     </td>
     </tr>
-  </table>
+</table>
+
+<h2>Books:</h2>
+
+@if (count($category->books) <=0)
+    <p>No records found</p>
+@endif
+
+<table id="categories">
+    <tr>
+      <th>Name</th>
+      <th>Author</th>
+      <th>Publication Date</th>
+      <th>Category</th>
+      <th>Edit</th>
+      <th>Delete</th>
+    </tr>
+    @foreach ($category->books as $book)
+    <tr>
+      <td>{{ $book->name }}</td>
+      <td>{{ $book->author }}</td>
+      <td>{{ $book->publication_date }}</td>
+      <td>{{ $book->category->name }}</td>
+
+      <td><a href="{{ url('books/' . $book->id . '/edit') }}">Edit</a></td>
+      <td>
+      <form action="{{ route('books.destroy', ['book' => $book]) }}" method="POST">
+        @csrf
+        @method('DELETE')
   
+        <button type="submit">Delete</button>
+      </form>
+      </td>
+    </tr>
+      @endforeach
+  </table>
+
 </body>
 </html>
