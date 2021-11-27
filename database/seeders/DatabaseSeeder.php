58297c294e7725ecad55b0d89b16796b81fcb342
Gustavo Navarro <rockeg18@gmail.com>
2021-11-27 13:42:54 -0600

    Update DatabaseSeeder.php
    Now It runs all factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(10)->create();
+        \App\Models\Book::factory(10)->create();
+        \App\Models\BookInstance::factory(10)->create();
     }
 }
