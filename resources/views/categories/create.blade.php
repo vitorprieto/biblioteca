2117d2fcf5a46daa78f4f175060ef5f512f4674e
Gustavo Navarro <rockeg18@gmail.com>
2021-11-27 14:56:11 -0600

    views/categories now using app layout

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,52 +1,60 @@
-<!DOCTYPE html>
-<html>
-<style>
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create a new category') }}
+        </h2>
+        <style>
+            input[type=text], select {
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+            }
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+            input[type=submit] {
+                width: 100%;
+                background-color: #4CAF50;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+            }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+            input[type=submit]:hover {
+                background-color: #45a049;
+            }
+            
+            div.create{
+                border-radius: 5px;
+                background-color: #f2f2f2;
+                padding: 20px;
+            }
+            
+        </style>
+    </x-slot>
 
-div {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>
-<body>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="create">
+                <form method="POST" action=" {{ route('categories.store') }}">
+                @csrf
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Name..">
 
-<h1>Create a new category</h1>
-<div>
-    <form method="POST" action=" {{ route('categories.store') }}">
-    @csrf
-      <label for="name">Name</label>
-      <input type="text" id="name" name="name" placeholder="Name..">
-
-      <label for="name">Description</label>
-      <input type="text" id="description" name="description" placeholder="Author..">
-    
-      <input type="submit" value="Submit">
-    </form>
+                <label for="name">Description</label>
+                <input type="text" id="description" name="description" placeholder="Author..">
+                
+                <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
-</body>
-</html>
+</x-app-layout>
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,59 +1,64 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-#categories {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Categories') }}
+        </h2>
+        <style>
+            #categories {
+                font-family: Arial, Helvetica, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+            
+            #categories td, #categories th {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+            
+            #categories tr:nth-child(even){background-color: #f2f2f2;}
+            
+            #categories tr:hover {background-color: #ddd;}
+            
+            #categories th {
+                padding-top: 12px;
+                padding-bottom: 12px;
+                text-align: left;
+                background-color: #04AA6D;
+                color: white;
+            }
+        </style>
+    </x-slot>
 
-#categories td, #categories th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
 
-#categories tr:nth-child(even){background-color: #f2f2f2;}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-#categories tr:hover {background-color: #ddd;}
-
-#categories th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #04AA6D;
-  color: white;
-}
-</style>
-</head>
-<body>
-
-<h1>Categories:</h1>
-
-<table id="categories">
-    <tr>
-      <th>Name</th>
-      <th>Description</th>
-      <th>Edit</th>
-      <th>Delete</th>
-    </tr>
-    @foreach ($categories as $category)
-    <tr>
-    <td>{{ $category->name }}</td>
-    <td>{{ $category->description }}</td>
-
-    <td><a href="{{ url('categories/' . $category->id . '/edit') }}">Edit</a></td>
-    <td>
-    <form action="{{ route('categories.destroy', ['category' => $category]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-
-      <button type="submit">Delete</button>
-    </form>
-    </td>
-    </tr>
-    @endforeach
-  </table>
-  
-</body>
-</html>
+                <table id="categories">
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    @foreach ($categories as $category)
+                    <tr>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ $category->description }}</td>
+                    
+                        <td><a href="{{ url('categories/' . $category->id . '/edit') }}">Edit</a></td>
+                        <td>
+                        <form action="{{ route('categories.destroy', ['category' => $category]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                    
+                        <button type="submit">Delete</button>
+                        </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
