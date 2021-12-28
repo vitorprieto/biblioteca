1abef5bffa84ac9178be6aa1c8a9e2ea553c4bd5
Gustavo Navarro <rockeg18@gmail.com>
2021-12-28 15:02:55 -0600

    Tailwindcss styles added to the table

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,70 +1,47 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Books') }}
         </h2>
-        <style>
-            #books {
-                font-family: Arial, Helvetica, sans-serif;
-                border-collapse: collapse;
-                width: 100%;
-            }
 
-            #books td,
-            #books th {
-                border: 1px solid #ddd;
-                padding: 8px;
-            }
-
-            #books tr:nth-child(even) {
-                background-color: #f2f2f2;
-            }
-
-            #books tr:hover {
-                background-color: #ddd;
-            }
-
-            #books th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #04AA6D;
-                color: white;
-            }
-
-        </style>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <table id="books">
-                    <tr>
-                        <th>Name</th>
-                        <th>Author</th>
-                        <th>Publication Date</th>
-                        <th>Category</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                    @foreach ($books as $book)
+    <div class="flex flex-col py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                <table class="min-w-full">
+                    <thead class="bg-gray-800 border-b">
                         <tr>
-                            <td>{{ $book->name }}</td>
-                            <td>{{ $book->author }}</td>
-                            <td>{{ $book->publication_date }}</td>
-                            <td>{{ $book->category->name }}</td>
-
-                            <td><a href="{{ url('books/' . $book->id . '/edit') }}">Edit</a></td>
-                            <td>
-                                <form action="{{ route('books.destroy', ['book' => $book]) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-
-                                    <button type="submit">Delete</button>
-                                </form>
-                            </td>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Name</th>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Author</th>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Publication Date</th>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Category</th>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Edit</th>
+                            <th scope="col" class="px-6 py-4 text-sm font-medium text-center text-white">Delete</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach ($books as $book)
+                            <tr class="transition duration-300 ease-in-out bg-white border-b hover:bg-gray-100">
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">{{ $book->name }}</td>
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">{{ $book->author }}</td>
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">{{ $book->publication_date }}</td>
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">{{ $book->category->name }}</td>
+
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                    <a href="{{ url('books/' . $book->id . '/edit') }}">Edit</a>
+                                </td>
+                                <td class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                    <form action="{{ route('books.destroy', ['book' => $book]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
             {{ $books->links() }}
